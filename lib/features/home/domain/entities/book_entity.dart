f6e8b59cd7a24e95bf7d51b8ebfab502cd0b7585
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2024-11-22 16:58:16 +0200

    book entity for data representation UI

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,17 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authorName;
+  final String description;
+  final num price;
+  final num rating;
+
+  BookEntity(
+    this.image,
+    this.title,
+    this.authorName,
+    this.description,
+    this.price,
+    this.rating,
+  );
+}
