718980913fe001530042611f8aeb0c2a58f1db31
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2024-12-09 15:56:55 +0200

    cache local data source newest books and refactor hive initialization code

---
+++ lib/core/utils/functions/hive_initialization.dart
@@ -0,0 +1,10 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:hive_flutter/adapters.dart';
+
+Future<void> hiveInitialization() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox<BookEntity>(kFeaturedBox);
+  await Hive.openBox<BookEntity>(kNewestBox);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_colors.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/functions/hive_initialization.dart';
 import 'package:bookly_app/core/utils/service_locator.dart';
-import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
@@ -10,15 +9,11 @@ import 'package:bookly_app/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   setupServiceLocator();
   Bloc.observer = SimpleBlocObserver();
-  await Hive.initFlutter();
-  Hive.registerAdapter(BookEntityAdapter());
-  await Hive.openBox<BookEntity>(kFeaturedBox);
-  await Hive.openBox<BookEntity>(kNewestBox);
+  await hiveInitialization();
   runApp(const BooklyApp());
 }
 
